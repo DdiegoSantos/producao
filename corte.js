c9ac664015f96245b1afd7ede8d3f2537b9945b4
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-06-15 14:26:47 -0300

    Update corte.js

--- corte.js
+++ corte.js
@@ -37,6 +37,22 @@ var misturar = document.querySelector("#misturar")
 var tq11N = document.querySelector("#tq11N");
 var tq13N = document.querySelector("#tq13N");
 
+//******* Torres ********
+var emb1 = document.querySelector("#emb1"), 
+    emb2 = document.querySelector("#emb2"),
+    emb3 = document.querySelector("#emb3"),
+    pesoEmb1 = document.querySelector("#pesoEmb1"),
+    pesoEmb2 = document.querySelector("#pesoEmb2"),
+    pesoEmb3 = document.querySelector("#pesoEmb3");
+var emb21 = document.querySelector("#emb2-1"),
+    emb22 = document.querySelector("#emb2-2"), 
+    emb23 = document.querySelector("#emb2-3"), 
+    pesoEmb21 = document.querySelector("#pesoEmb2-1"), 
+    pesoEmb22 = document.querySelector("#pesoEmb2-2"), 
+    pesoEmb23 = document.querySelector("#pesoEmb2-3");
+
+
+
 var resultado = document.querySelector("#resultado");
 
 function bmistura(x,y){
@@ -56,51 +72,55 @@ function multiplicar(x,y){
 
 function calcular(){
     //**** calcula o PCP ****
-    res1 = multiplicar(Number(tor.value), Number(red.value));
-    res2 = multiplicar(Number(brixD.value), Number(dadosD.value));
-    res3 = multiplicar(Number(dadosC.value), Number(brixC.value));
-
+    var 
+    res1 = multiplicar(Number(tor.value), Number(red.value)),
+    res2 = multiplicar(Number(brixD.value), Number(dadosD.value)),
+    res3 = multiplicar(Number(dadosC.value), Number(brixC.value)),
     
-    let vazao = 2200;
-    let prod1, prod2, prod3;
+    torre1 = Number(emb1.value)*Number(pesoEmb1.value) + Number(emb2.value)*Number(pesoEmb2.value) + Number(emb3.value)*Number(pesoEmb3.value),
+    torre2 = Number(emb21.value)*Number(pesoEmb21.value) + Number(emb22.value)*Number(pesoEmb22.value) + Number(emb23.value)*Number(pesoEmb23.value)
+
     prod1 = Number(pcp1.value)*Number(peso1.value);
     prod2 = Number(pcp2.value)*Number(peso2.value);
     prod3 = Number(pcp3.value)*Number(peso3.value);
+    
+    let vazao = 2200, vazTaste = 13000, relExt = 6.75;
+    var prod1, prod2, prod3, tempEx, pcp, torr, Rtorr, fTor;
 
-    somaPcp.innerHTML = `Total dessa Liga ${(prod1 + prod2 + prod3).toFixed(2)}`;
+    totPo = res1 + res2 + res3 + torre1 + torre2;
+    Rtorr = Number(red.value)/100;
+    torr = Number(tor.value)/vazao;
+    pcp = prod1 + prod2 + prod3;
+    taste = (fTor + Number(tor.value))/relExt;
+    
 
-    //**** Calcula o tempo de Extrair ****
-    
-    
-    var fTor = (prod1 + prod2 + prod3) - (res1 + res2 + res3);
-    
-    var tempEx = (((prod1+prod2+prod3)-(res1+res2+res3))/(Number(red.value)/100)+Number(tor.value))/vazao;
-    
-    tempoEx.innerHTML = `Extração: ${tempEx.toFixed(2)} horas <br> Concentrador: ${fTor.toFixed(2)} horas`;
+    somaPcp.innerHTML = `Total dessa Liga ${(pcp).toFixed(2)}`;
 
+    
+   
     //**** checa outras Ligas ****
     if(fliga[0].checked){
-        var pcp,totPo,Rtorr,torr;
-        pcp = prod1 + prod2 + prod3;
-        totPo = res1+res2+res3;
-        Rtorr = Number(red.value)/100;
-        torr = Number(tor.value)/vazao;
-        var tempEx = (pcp - totPo) / Rtorr + torr; 
-               
+        fTor = (pcp - totPo) / Rtorr;
+        tempEx = fTor + torr ;
+        tempoEx.innerHTML = `Falta à torrar ${fTor.toFixed(2)} (${(fTor/ 450).toFixed(2)} cargas); <br> Extração: ${tempEx.toFixed(2)} horas <br> Concentrador: ${(fTor/vazTaste).toFixed(2)} horas`;
+
         resultado.innerHTML = `${totPo}kg de pó`;
         resultado.tofixed(2);
-    }else{
-        var pcp,totPo,Rtorr,torr;
-        pcp = prod1 + prod2 + prod3;
-        totPo = res1+res2+res3;
-        torr = Number(red.value)/100;
-        torr = Number(tor.value)/vazao;
-        var tempEx = (pcp - totPo) / Rtorr + torr; 
 
-        res4 = bmistura(Number(tq11N.value), Number(tq13N.value))
         
+    
+    }else{        
+        var tempEx = (pcp - totPo) / Rtorr + torr; 
+        res4 = bmistura(Number(tq11N.value), Number(tq13N.value));        
         resultado.innerHTML = `${(totPo + res4).toFixed(2)} kg de pó` ;
+
+        fTor = (pcp - totPo) / Rtorr;
+        tempEx = (((pcp-totPo)/Rtorr) + torr + res4)/vazao;  
+        tempoEx.innerHTML = `Falta à torrar ${fTor.toFixed(2)} (${(fTor/ 450).toFixed(2)} cargas); <br> Extração: ${tempEx.toFixed(2)} horas <br> Concentrador: ${fTor.toFixed(2)} horas`; 
     }
+     
+         
+
 }
 
 function aparecer(){
