30b8c9c6da331446af6f3d462b41ca14158161dc
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-06-15 14:39:54 -0300

    Update corte.js

--- corte.js
+++ corte.js
@@ -116,7 +116,7 @@ function calcular(){
 
         fTor = (pcp - totPo) / Rtorr;
         tempEx = (((pcp-totPo)/Rtorr) + torr + res4)/vazao;  
-        tempoEx.innerHTML = `Falta à torrar ${fTor.toFixed(2)} (${(fTor/ 450).toFixed(2)} cargas); <br> Extração: ${tempEx.toFixed(2)} horas <br> Concentrador: ${fTor.toFixed(2)} horas`; 
+        tempoEx.innerHTML = `Falta à torrar ${fTor.toFixed(2)} (${(fTor/ 450).toFixed(2)} cargas); <br> Extração: ${tempEx.toFixed(2)} horas <br> Concentrador: ${((fTor+res4)/vazTaste).toFixed(2)} horas`; 
     }
      
          
